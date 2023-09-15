d1ecd5988d39d924838a7af2607dfde9252e8162
dashatretyakk <99836122+dashatretyakk@users.noreply.github.com>
2023-09-15 23:42:02 +0300

    Update ThreadExampleA.java

--- ThreadExampleA.java
+++ ThreadExampleA.java
@@ -4,9 +4,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ThreadExampleA {
-    private static Thread thread1; 
-    private static Thread thread2; 
-    private static JSlider slider; 
+    private static Thread thread1; // Перший потік
+    private static Thread thread2; // Другий потік
+    private static JSlider slider; // Повзунок для візуалізації потоків
     private static JSpinner prioritySpinner1; // Спіннер для вибору пріоритету першого потоку
     private static JSpinner prioritySpinner2; // Спіннер для вибору пріоритету другого потоку
 
@@ -15,37 +15,37 @@ public class ThreadExampleA {
         frame.setLayout(new FlowLayout()); // Встановлення менеджера розміщення FlowLayout
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Дія при закритті вікна
 
-        slider = new JSlider(0, 100); 
-        slider.setMajorTickSpacing(10); 
-        slider.setMinorTickSpacing(10); 
-        slider.setPaintTicks(true); 
-        slider.setPaintLabels(true); 
-        slider.setPreferredSize(new Dimension(250, slider.getPreferredSize().height)); 
+        slider = new JSlider(0, 100); // Створення ползунка
+        slider.setMajorTickSpacing(10); // Встановлення великої поділки
+        slider.setMinorTickSpacing(10); // Встановлення малої поділки
+        slider.setPaintTicks(true); // Відображення поділок на ползунку
+        slider.setPaintLabels(true); // Відображення значень на ползунку
+        slider.setPreferredSize(new Dimension(250, slider.getPreferredSize().height)); // Встановлення розмірів ползунка
 
         JPanel sliderPanel = new JPanel(); // Панель для ползунка
         sliderPanel.setLayout(new BoxLayout(sliderPanel, BoxLayout.Y_AXIS)); // Встановлення менеджера розміщення BoxLayout
-        sliderPanel.add(slider); 
+        sliderPanel.add(slider); // Додавання ползунка на панель
 
-        prioritySpinner1 = new JSpinner(new SpinnerNumberModel(5, 1, 10, 1)); 
-        prioritySpinner2 = new JSpinner(new SpinnerNumberModel(5, 1, 10, 1)); 
+        prioritySpinner1 = new JSpinner(new SpinnerNumberModel(5, 1, 10, 1)); // Спіннер для пріоритету першого потоку
+        prioritySpinner2 = new JSpinner(new SpinnerNumberModel(5, 1, 10, 1)); // Спіннер для пріоритету другого потоку
 
         JPanel spinnerPanel = new JPanel(); // Панель для спіннерів
         spinnerPanel.setLayout(new FlowLayout()); // Встановлення менеджера розміщення FlowLayout
-        spinnerPanel.add(new JLabel("Пріоритет 1:")); 
-        spinnerPanel.add(prioritySpinner1); 
-        spinnerPanel.add(new JLabel("Пріоритет 2:")); 
-        spinnerPanel.add(prioritySpinner2); 
+        spinnerPanel.add(new JLabel("Пріоритет 1:")); // Додавання напису "Пріоритет 1" на панель
+        spinnerPanel.add(prioritySpinner1); // Додавання спіннера для пріоритету першого потоку на панель
+        spinnerPanel.add(new JLabel("Пріоритет 2:")); // Додавання напису "Пріоритет 2" на панель
+        spinnerPanel.add(prioritySpinner2); // Додавання спіннера для пріоритету другого потоку на панель
 
-        JButton startButton = new JButton("Пуск"); 
+        JButton startButton = new JButton("Пуск"); // Кнопка "Пуск"
 
         startButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                int priority1 = (int) prioritySpinner1.getValue(); 
-                int priority2 = (int) prioritySpinner2.getValue(); 
+                int priority1 = (int) prioritySpinner1.getValue(); // Отримання значення пріоритету першого потоку
+                int priority2 = (int) prioritySpinner2.getValue(); // Отримання значення пріоритету другого потоку
 
                 if (thread1 != null && thread1.isAlive()) { // Перевірка чи існує і живий перший потік
-                    thread1.setPriority(priority1); 
+                    thread1.setPriority(priority1); // Встановлення пріоритету першого потоку
                 } else {
                     thread1 = new Thread(new Runnable() {
                         @Override
@@ -55,8 +55,8 @@ public class ThreadExampleA {
                             }
                         }
                     });
-                    thread1.setPriority(priority1); 
-                    thread1.start(); 
+                    thread1.setPriority(priority1); // Встановлення пріоритету першого потоку
+                    thread1.start(); // Запуск першого потоку
                 }
 
                 if (thread2 != null && thread2.isAlive()) { // Перевірка чи існує і живий другий потік
@@ -71,17 +71,18 @@ public class ThreadExampleA {
                         }
                     });
                     thread2.setPriority(priority2); // Встановлення пріоритету другого потоку
-                    thread2.start(); 
+                    thread2.start(); // Запуск другого потоку
                 }
             }
         });
 
-        frame.add(sliderPanel); 
-        frame.add(spinnerPanel); 
-        frame.add(startButton); 
-        frame.setSize(300, 180); // 
-        frame.setLocationRelativeTo(null); 
-        frame.setResizable(false); 
-        frame.setVisible(true); 
+        frame.add(sliderPanel); // Додавання панелі з ползунком на вікно
+        frame.add(spinnerPanel); // Додавання панелі зі спіннерами на вікно
+        frame.add(startButton); // Додавання кнопки "Пуск" на вікно
+
+        frame.setSize(300, 180); // Встановлення розмірів вікна
+        frame.setLocationRelativeTo(null); // Встановлення положення вікна по центру екрана
+        frame.setResizable(false); // Заборона зміни розміру вікна
+        frame.setVisible(true); // Відображення вікна
     }
 }
